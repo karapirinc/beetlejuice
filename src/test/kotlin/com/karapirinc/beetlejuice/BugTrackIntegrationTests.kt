e397cb0f3d265a37dc77fa3131796035c5417127
Yusuf Ismail Oktay <yio@viabill.com>
2022-09-07 16:48:13 +0300

    add core functionality test scenarios

---
+++ src/test/kotlin/com/karapirinc/beetlejuice/BugTrackIntegrationTests.kt
@@ -0,0 +1,34 @@
+package com.karapirinc.beetlejuice
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BugTrackIntegrationTests {
+
+    @Test
+    fun `should report a bug`() {
+        fail("Not implemented")
+    }
+
+    @Test
+    fun `should update a bug`() {
+        fail("Not implemented")
+    }
+
+    @Test
+    fun `should delete a bug`() {
+        fail("Not implemented")
+    }
+
+    @Test
+    fun `should fetch a bug`() {
+        fail("Not implemented")
+    }
+
+    @Test
+    fun `should search a bug`() {
+        fail("Not implemented")
+    }
+}
